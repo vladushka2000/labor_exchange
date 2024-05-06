d42fcbf9cf6dfd031022e06cf2d7e7c560155e78
vladushka2000 <nazipovvt@yandex.ru>
2024-05-06 23:11:36 +0500

    refactor(main) - Добавлена точка входа в программу

--- src/main.py
+++ src/main.py
@@ -1,28 +1,10 @@
-from fastapi import FastAPI, Request, status
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-from routers import auth, user, job, response
+from fastapi import FastAPI
 import uvicorn
 
-app = FastAPI()
-app.include_router(auth.router)
-app.include_router(user.router)
-app.include_router(job.router)
-app.include_router(response.router)
+from entrypoints import router_registrator
 
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    return JSONResponse(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content=jsonable_encoder({"Ошибка валидации входных данных": str(exc)}),
-    )
-
-
-@app.get("/")
-def hello():
-    return {"message": "Hello, world!"}
+app = FastAPI(title="labor exchange")
+router_registrator.register_routes(app)
 
 
 if __name__ == '__main__':
