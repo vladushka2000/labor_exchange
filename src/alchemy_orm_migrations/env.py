de7bb54043cc3edc636f68b64161e4b9d308832a
vladushka2000 <nazipovvt@yandex.ru>
2024-05-06 23:10:52 +0500

    refactor(alembic) - Создана миграция

--- src/alchemy_orm_migrations/env.py
+++ src/alchemy_orm_migrations/env.py
@@ -1,14 +1,14 @@
 import asyncio
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import AsyncEngine
-from db_settings import DB_NAME, DB_HOST, DB_PASS, DB_USER, Base
 
-from alembic import context
-from models import *
+from repositories.alchemy_orm.common import Base
+from repositories.alchemy_orm.common.db_settings import DB_NAME, DB_HOST, DB_PASS, DB_USER
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -39,7 +39,7 @@ target_metadata = [Base.metadata]
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
+    """Run alchemy_orm_migrations in 'offline' mode.
 
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
@@ -70,7 +70,7 @@ def do_run_migrations(connection: Connection) -> None:
 
 
 async def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+    """Run alchemy_orm_migrations in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
